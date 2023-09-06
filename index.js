a22ef7895fe8346b3660393ea3689f27b8e604b3
DH <shim5013@naver.com>
2023-09-06 11:28:01 +0900

    09.06

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.get('/sound/:animal', function (req, res) {
+    const { animal } = req.params
+
+    if (animal == "dog") {
+        res.json({ 'sound': '왕왕' })
+    } else if (animal == "cat") {
+        res.json({ 'sound': '야옹' })
+    } else if (animal == "pig") {
+        res.json({ 'sound': '꿀꿀' })
+    } else {
+        res.json({ 'sound': '알 수 없음' })
+    }
+})
+
+app.listen(3000)
